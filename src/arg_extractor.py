94582982deee26adc8c454b016f8c3db55f69cda
s1928467 <n.debono@hotmail.com>
2020-02-14 15:00:57 +0000

    added arguments in parser

--- src/arg_extractor.py
+++ src/arg_extractor.py
@@ -53,6 +53,20 @@ def get_args():
                         help='Weight decay to use for Adam')
     parser.add_argument('--filepath_to_arguments_json_file', nargs="?", type=str, default=None,
                         help='')
+    parser.add_argument('--learn_rate_max', nargs="?", type=float, default=0.0001,
+                        help='Starting learning rate. Max learning rate in a lr scheduler')
+    parser.add_argument('--learn_rate_min', nargs="?", type=float, default=0.00001,
+                        help='Min learning rate in a lr scheduler')
+    parser.add_argument('--optim-type', nargs="?", type=str, default='Adam',
+                        help='Which optimiser to use (Adam, SGD)')
+    parser.add_argument('--momentum', nargs="?", type=float, default=0.9,
+                        help='Which optimiser to use (Adam, SGD)')
+    parser.add_argument('--sched-type', nargs="?", type=str, default=None,
+                        help='Which learn rate scheduler to use (ERF)')
+    parser.add_argument('--erf-sched-alpha', nargs="?", type=int, default=None,
+                        help='ERF alpha hyperparam')
+    parser.add_argument('--erf-sched-beta', nargs="?", type=int, default=None,
+                        help='ERF beta hyperparam')
 
     args = parser.parse_args()
 
