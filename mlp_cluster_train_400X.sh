31cb084010be025226434d556b735273c0a3c12e
ferendo <dylanfrendo@gmail.com>
2020-02-19 13:04:51 +0000

    Updated batch size

--- mlp_cluster_train_400X.sh
+++ mlp_cluster_train_400X.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Teach-Standard
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-04:00:00
 
@@ -38,10 +38,10 @@ tar -xzf "${DATASET_DIR}/BreaKHis_v1.tar.gz" -C "${DATASET_DIR}"
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python ./src/train.py --batch_size 20 --continue_from_epoch -1 --seed 0 \
+python ./src/train.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
                                                       --image_num_channels 3 --image_height 224 --image_width 224 \
                                                       --num_layers 3 --num_filters 16 \
-                                                      --num_epochs 100 --experiment_name 'base_tune_test_1_400X' \
+                                                      --num_epochs 100 --experiment_name 'base_tune_test_100_400X' \
                                                       --use_gpu "True" --weight_decay_coefficient 0.0001 \
                                                       --optim_type "SGD" --momentum 0.9 --nesterov "True" \
                                                       --sched_type "ERF" --learn_rate_max 0.01 --learn_rate_min 0.0001 \
