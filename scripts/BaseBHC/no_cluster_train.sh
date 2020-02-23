c2ed03855312b63d342d360ad8521cdb40404be5
ferendo <dylanfrendo@gmail.com>
2020-02-23 21:31:48 +0000

    Increased the number of layers

--- scripts/BaseBHC/no_cluster_train.sh
+++ scripts/BaseBHC/no_cluster_train.sh
@@ -22,7 +22,7 @@ do
 
     python ../../src/main.py --use_gpu "True" --batch_size 20 --num_epochs 100 --continue_from_epoch -1 --seed 0 \
                          --image_num_channels 3 --image_height 224 --image_width 224 \
-                         --num_layers 3 --num_filters 16 \
+                         --num_layers 6 --num_filters 16 \
                          --dataset_location "${DATASET_DIR}" --experiment_name "${experiment_result_location}" \
                          --optim_type "SGD" --momentum 0.9 --nesterov "True" --weight_decay_coefficient 0.0001 \
                          --sched_type "ERF" --learn_rate_max 0.01 --learn_rate_min 0.0001 \
