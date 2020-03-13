4510189eb1b2ea7dfe4a3f2b0d9cf2f2b23b9939
ferendo <dylanfrendo@gmail.com>
2020-03-13 17:00:52 +0000

    Updated script

--- scripts/FixMatchDenseNet/mlp_cluster_train.sh
+++ scripts/FixMatchDenseNet/mlp_cluster_train.sh
@@ -33,7 +33,10 @@ export DATASET_DIR=${TMP}/datasets
 
 # Activate the relevant virtual environment:
 rsync -ua /home/${STUDENT_ID}/Leveraging-Unlabeled-Data-For-Breast-Cancer-Classification/data/BreaKHis_v1.tar.gz "${DATASET_DIR}"
-tar -xzf "${DATASET_DIR}/BreaKHis_v1.tar.gz" -C "${DATASET_DIR}"
+
+if [ ! -d "${DATASET_DIR}/BreaKHis_v1/" ]; then
+  tar -xzf "${DATASET_DIR}/BreaKHis_v1.tar.gz" -C "${DATASET_DIR}"
+fi
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
