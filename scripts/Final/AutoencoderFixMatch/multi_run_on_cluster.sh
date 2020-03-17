d7e7b206ca2e91b8c592e406ef14764d282290b1
Dylan <dylanfrendo@gmail.com>
2020-03-17 21:36:29 +0000

    Added location of the pre-trained models

--- scripts/Final/AutoencoderFixMatch/multi_run_on_cluster.sh
+++ scripts/Final/AutoencoderFixMatch/multi_run_on_cluster.sh
@@ -2,7 +2,11 @@
 
 seeds=(4324 342432 764365)
 magnifications=("40X" "100X" "200X" "400X")
-autoencoder_locations=("temp1" "temp2" "temp3" "temp4")
+autoencoder_locations=("../Autoencoder/experiments/autoencoder_342432_40X/saved_models/" \
+                       "../Autoencoder/experiments/autoencoder_4324_100X/saved_models/" \
+                       "../Autoencoder/experiments/autoencoder_764365_200X/saved_models/" \
+                       "../Autoencoder/experiments/autoencoder_342432_400X/saved_models/")
+                       
 labeled_images_amount=(5 10 20 50)
 
 for seed in "${seeds[@]}"
