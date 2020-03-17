caa954b40ed7aa65c681d01fa133ae335a0fa508
Dylan <dylanfrendo@gmail.com>
2020-03-17 17:57:19 +0000

    Added scripts for fix match final

---
+++ scripts/Final/FixMatch/multi_run_on_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+seeds=(4324 342432 764365)
+magnifications=("40X" "100X" "200X" "400X")
+labeled_images_amount=(2 5 10 20 50)
+
+for seed in "${seeds[@]}"
+do
+  for magnification in "${magnifications[@]}"
+  do
+    for labeled_images in "${labeled_images_amount[@]}"
+    do
+      experiment_result_location="./experiments/autoencoder_${seed}_${magnification}_${labeled_images}"
+
+      sbatch mlp_cluster_train.sh ${seed} "${magnification}" ${labeled_images} "${experiment_result_location}"
+    done
+  done
+done
